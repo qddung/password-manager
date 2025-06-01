fce013657ff4afb63f65177536917c2c8d294ca7
dung <tranquocdung1998.i3@gmail.com>
2025-06-01 11:52:42 +0700

    example seal walrus

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { StyleProvider } from '@ant-design/cssinjs';
-import { ConfigProvider } from 'antd';
-// import './index.css'
-import App from './App.tsx';
-import { AppContextProvider } from './context/AppContext.tsx';
-import { createNetworkConfig, lightTheme, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@mysten/dapp-kit/dist/index.css';
+import '@radix-ui/themes/styles.css';
+
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { getFullnodeUrl } from '@mysten/sui/client';
+import { Theme } from '@radix-ui/themes';
+import App from './App';
+import { networkConfig } from './networkConfig';
 
-const { networkConfig } = createNetworkConfig({
-  mainnet: { url: getFullnodeUrl('mainnet') },
-});
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }
-});
-
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <SuiClientProvider networks={networkConfig} defaultNetwork="mainnet">
-      <WalletProvider
-        theme={lightTheme}
-        autoConnect={true}
-        storage={localStorage}
-        storageKey="sui-wallet"
-        preferredWallets={["Sui Wallet"]}
-      >
-        <StyleProvider layer>
-          <ConfigProvider theme={{
-            // token: {
-            //   colorPrimary: '#ff97d6'
-            // }
-          }}>
-            <AppContextProvider>
-              <App />
-            </AppContextProvider>
-          </ConfigProvider>
-        </StyleProvider>
-      </WalletProvider >,
-    </SuiClientProvider >,
-  </QueryClientProvider >,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Theme appearance="dark">
+      <QueryClientProvider client={queryClient}>
+        <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+          <WalletProvider autoConnect>
+            <App />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
+    </Theme>
+  </React.StrictMode>,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,82 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc';
-import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      '@/': `${path.resolve(__dirname, 'src')}/`
-    }
+  server: {
+    proxy: {
+      '/aggregator1/v1': {
+        target: 'https://aggregator.walrus-testnet.walrus.space',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator1/, ''),
+      },
+      '/aggregator2/v1': {
+        target: 'https://wal-aggregator-testnet.staketab.org',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator2/, ''),
+      },
+      '/aggregator3/v1': {
+        target: 'https://walrus-testnet-aggregator.redundex.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator3/, ''),
+      },
+      '/aggregator4/v1': {
+        target: 'https://walrus-testnet-aggregator.nodes.guru',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator4/, ''),
+      },
+      '/aggregator5/v1': {
+        target: 'https://aggregator.walrus.banansen.dev',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator5/, ''),
+      },
+      '/aggregator6/v1': {
+        target: 'https://walrus-testnet-aggregator.everstake.one',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/aggregator6/, ''),
+      },
+      '/publisher1/v1': {
+        target: 'https://publisher.walrus-testnet.walrus.space',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher1/, ''),
+      },
+      '/publisher2/v1': {
+        target: 'https://wal-publisher-testnet.staketab.org',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher2/, ''),
+      },
+      '/publisher3/v1': {
+        target: 'https://walrus-testnet-publisher.redundex.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher3/, ''),
+      },
+      '/publisher4/v1': {
+        target: 'https://walrus-testnet-publisher.nodes.guru',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher4/, ''),
+      },
+      '/publisher5/v1': {
+        target: 'https://publisher.walrus.banansen.dev',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher5/, ''),
+      },
+      '/publisher6/v1': {
+        target: 'https://walrus-testnet-publisher.everstake.one',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/publisher6/, ''),
+      },
+    },
   },
-  define: {
-    // eslint-disable-next-line no-undef
-    'process.env': process.env
-  }
-})
+});
